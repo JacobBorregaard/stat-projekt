1e8d34166c3d043c409ae7f01daa613bdce89529
Jacob pc <jacob.bore>
2023-01-07 18:28:56 +0100

    file added

--- module1.py
+++ module1.py
@@ -1,2 +1,8 @@
+import pandas as pd
 
-def hej:
+
+filename = "horse_data23.txt"
+
+df = pd.read_csv(filename, sep=" ")
+
+#er ved at prøve og finde ud af hvordan vi reader filen ordentligt det her virker ikke
